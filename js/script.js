83b7c6b44a9a01511ea6568cc2f8d9ee1c0755c7
Carla Borges Reis <carlareis.dev.com>
2023-10-07 14:19:40 -0700

    fixed modal picture, phone number and dob

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function getEmployeesData(url) {
             employees = data.results;
             displayEmployees(employees);
             })
+        .then((data) => console.log(employees))
         .catch((err) => console.log(err))
 }
 
@@ -33,27 +34,41 @@ function displayEmployees(data) {
 }
 
 function showModal(employee) {
+    function normalize(phone) {
+        //normalize string and remove all unnecessary characters
+        phone = phone.replace(/[^\d]/g, "");
+    
+        //check if number length equals to 10
+        if (phone.length == 10) {
+            //reformat and return phone number
+            return phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+        }
+        return phone;
+    }
+
+    const birthday = new Date(employee.dob.date);
+
     const modalHTML = `
         <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.email}" alt="${employee.name.first} ${employee.name.last}">
+                    <img class="modal-img" src="${employee.picture.thumbnail}" alt="${employee.name.first} ${employee.name.last}">
                     <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
-                    <p class="modal-text">${employee.cell}</p>
+                    <p class="modal-text">${normalize(employee.cell)}</p>
                     <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${employee.dob.date.getMonth + 1}/${employee.dob.date.getDay}/${employee.dob.date.getYear}</p>
+                    <p class="modal-text">Birthday: ${birthday.getMonth() + 1}/${birthday.getDay()}/${birthday.getYear()}</p>
                 </div>
             </div>
 
-            // IMPORTANT: Below is only for exceeds tasks 
-            // <div class="modal-btn-container">
-            //     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            //     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-            // </div>
+            //IMPORTANT: Below is only for exceeds tasks 
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
         </div>
     `;
     body.insertAdjacentHTML('beforeend', modalHTML);
