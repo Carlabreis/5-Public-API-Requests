79572db8208f85c31c02d9b5dbeae5ff1240809a
Carla Borges Reis <carlareis.dev.com>
2023-10-09 10:10:36 -0700

    add event listener to close modal

--- js/script.js
+++ js/script.js
@@ -1,18 +1,44 @@
 const gallery = document.getElementById("gallery");
 const body = document.querySelector("body");
+
 let employees = [];
 
 function getEmployeesData(url) {
-    fetch(url)
-        .then((res) => res.json())
-        // .then(data => console.log(data))
-        .then((data) => {
-            employees = data.results;
-            displayEmployees(employees);
-            })
-        .then((data) => console.log(employees))
-        .catch((err) => console.log(err))
+  fetch(url)
+    .then((res) => res.json())
+    // .then(data => console.log(data))
+    .then((data) => {
+      employees = data.results;
+      displayEmployees(employees);
+    })
+    // .then((data) => console.log(employees))
+    .catch((err) => console.log(err));
 }
+
+gallery.addEventListener("click", (e) => {
+  const card = e.target.closest(".card");
+  const nameTarget = card.querySelector("#name").textContent;
+
+  const employee = employees.find(
+    (employee) => employee.name.first + " " + employee.name.last === nameTarget
+  );
+
+  showModal(employee);
+});
+
+body.addEventListener("click", (e) => {
+  if (e.target.closest("#modal-close-btn")) {
+    const modalContainer = document.querySelector(".modal-container");
+    modalContainer.remove();
+  } else if (
+    e.target.closest(".modal-container") &&
+    !e.target.closest(".modal") &&
+    !e.target.closest(".modal-btn-container")
+  ) {
+    const modalContainer = document.querySelector(".modal-container");
+    modalContainer.remove();
+  }
+});
 
 function displayEmployees(data) {
   const galleryHTML = data
@@ -28,39 +54,50 @@ function displayEmployees(data) {
                 <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
         </div>
-    `)
+    `
+    )
     .join("");
   gallery.insertAdjacentHTML("beforeend", galleryHTML);
 }
 
 function showModal(employee) {
-    function normalize(phone) {
-        //normalize string and remove all unnecessary characters
-        phone = phone.replace(/[^\d]/g, "");
-    
-        //check if number length equals to 10
-        if (phone.length == 10) {
-            //reformat and return phone number
-            return phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
-        }
-        return phone;
+  function normalize(phone) {
+    //normalize string and remove all unnecessary characters
+    phone = phone.replace(/[^\d]/g, "");
+
+    //check if number length equals to 10
+    if (phone.length == 10) {
+      //reformat and return phone number
+      return phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
     }
+    return phone;
+  }
 
-    const birthday = new Date(employee.dob.date);
+  const birthday = new Date(employee.dob.date);
 
-    const modalHTML = `
+  const modalHTML = `
         <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.thumbnail}" alt="${employee.name.first} ${employee.name.last}">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <img class="modal-img" src="${
+                      employee.picture.thumbnail
+                    }" alt="${employee.name.first} ${employee.name.last}">
+                    <h3 id="name" class="modal-name cap">${
+                      employee.name.first
+                    } ${employee.name.last}</h3>
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
                     <p class="modal-text">${normalize(employee.cell)}</p>
-                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${birthday.getMonth() + 1}/${birthday.getDay()}/${birthday.getYear()}</p>
+                    <p class="modal-text">${employee.location.street.number} ${
+    employee.location.street.name
+  }, ${employee.location.city}, ${employee.location.state} ${
+    employee.location.postcode
+  }</p>
+                    <p class="modal-text">Birthday: ${
+                      birthday.getMonth() + 1
+                    }/${birthday.getDay()}/${birthday.getYear()}</p>
                 </div>
             </div>
 
@@ -71,16 +108,7 @@ function showModal(employee) {
             </div>
         </div>
     `;
-    body.insertAdjacentHTML('beforeend', modalHTML);
+  body.insertAdjacentHTML("beforeend", modalHTML);
 }
 
-body.addEventListener("click", (e) => {
-    const card = e.target.closest(".card");
-    const nameTarget = card.querySelector("#name").textContent;
-
-    const employee = employees.find((employee) => employee.name.first + " " + employee.name.last === nameTarget);
-    
-    showModal(employee);
-})
-
-getEmployeesData("https://randomuser.me/api/?results=20");
+getEmployeesData("https://randomuser.me/api/?results=12");
